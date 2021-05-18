5643a4b2ff3be28138f65aa61ae403c8a9070184
huak <komsark_@hotmail.co.th>
2021-05-18 23:07:05 +0700

    commit

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -738,4 +738,4 @@ decoder.eval()
 searcher = GreedySearchDecoder(encoder, decoder)
 
 # Begin chatting (uncomment and run the following line to begin)
-# evaluateInput(encoder, decoder, searcher, voc)
+evaluateInput(encoder, decoder, searcher, voc)
